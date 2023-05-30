b3b7694a4388122935dad69e65268b3240014f69
feXXon <RobertGuzaerov123@yandex.ru>
2023-05-30 11:51:47 +0300

    Добавьте файлы проекта.

---
+++ WpfApp35/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp35
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int[] a = Boxa.Text.Split().Select(int.Parse).ToArray();
+            int x = a.Where(n => n % 6 == 0 && n % 10 == 4).Sum();
+            txtOtv.Text = $"{x}";
+        }
+    }
+}
